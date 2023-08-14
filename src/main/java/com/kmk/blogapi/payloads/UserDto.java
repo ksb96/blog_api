87c9f0f4b9f72ea26974c6350e28db62047852d6
ksb96 <52975077+ksb96@users.noreply.github.com>
2023-08-14 15:42:34 +0530

    userdto api --D

--- src/main/java/com/kmk/blogapi/payloads/UserDto.java
+++ src/main/java/com/kmk/blogapi/payloads/UserDto.java
@@ -1,5 +1,9 @@
 package com.kmk.blogapi.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +14,15 @@ import lombok.Setter;
 public class UserDto {
 
 	private int id;
+	@NotEmpty
+	@Size(min = 4, message = "Username must be of 4 characters")
 	private String name;
+	@Email(message = "Email is not valid!!")
 	private String email;
+	@NotEmpty
+	@Size(min = 4, max = 10, message = "Password must be min of 3 characters and max of 10 characters")
 	private String password;
+	@NotEmpty
 	private String about;
 
 }
