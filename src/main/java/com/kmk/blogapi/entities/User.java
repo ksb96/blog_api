b2f78d773237f5d59dc38652120d002bfb657fb9
ksb96 <52975077+ksb96@users.noreply.github.com>
2023-10-29 13:17:51 +0530

    user_role & join table --D

--- src/main/java/com/kmk/blogapi/entities/User.java
+++ src/main/java/com/kmk/blogapi/entities/User.java
@@ -1,7 +1,9 @@
 package com.kmk.blogapi.entities;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.annotation.Generated;
 import jakarta.persistence.CascadeType;
@@ -11,6 +13,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -23,24 +28,28 @@ import lombok.Setter;
 @Setter
 @Getter
 public class User {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
 	@Column(name = "user_id")
-	private int id;	
-	
+	private int id;
+
 	@Column(name = "user_name", nullable = false, length = 20)
 	private String name;
-	
+
 	@Column(name = "user_email", nullable = false, length = 20)
 	private String email;
-	
+
 	@Column(nullable = false, length = 20)
 	private String password;
-	
+
 	@Column(nullable = true, length = 50)
 	private String about;
-	
+
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Post> posts = new ArrayList<>();
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user", referencedColumnName = "user_id"), inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
+	private Set<Role> roles = new HashSet<>();
 }
