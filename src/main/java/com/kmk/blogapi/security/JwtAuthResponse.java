faf6ff1e37d54b5613e7beff96570acf2a7e24d9
ksb96 <52975077+ksb96@users.noreply.github.com>
2023-11-27 23:07:21 +0530

    jwt auth response class --I

---
+++ src/main/java/com/kmk/blogapi/security/JwtAuthResponse.java
@@ -0,0 +1,9 @@
+package com.kmk.blogapi.security;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponse {
+
+	private String token;
+}
--- src/main/java/com/kmk/blogapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kmk/blogapi/security/JwtAuthenticationFilter.java
@@ -3,44 +3,78 @@ package com.kmk.blogapi.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
-	
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JwtTokenHelper jwtTokenHelper;
 
 	@Override
-	//this will get call whenever an api request will get hit
+	// this will get call whenever an api request will get hit
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
-		//get token
+
+		// get token
 		String requestToken = request.getHeader("Authorization");
-		
-		//bearer 2343434erdfdf4
-		System.out.println(requestToken);
-		
+
+		// bearer 2343434erdfdf4
+//		System.out.println(requestToken);
+
 		String username = null;
 		String token = null;
-		
-		if(request!=null && requestToken.startsWith("Bearer")) {
-			
-			
-		}else {
+
+		if (requestToken != null && requestToken.startsWith("Bearer")) {
+			token = requestToken.substring(7);
+			try {
+				username = this.jwtTokenHelper.getUsernameFromToken(token);
+			} catch (IllegalArgumentException e) {
+				System.out.println("Unable to get token");
+			} catch (ExpiredJwtException e) {
+				System.out.println("token expired");
+			} catch (MalformedJwtException e) {
+				System.out.println("invalid token!!");
+			}
+
+		} else {
 			System.out.println("Jwt token doesn't starts with Bearer");
 		}
+
+		// validate above token
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+			if (this.jwtTokenHelper.validateToken(token, userDetails)) {
+				// need to authenticate & set it
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken
+						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			} else {
+				System.out.println("Invalid token");
+			}
+		} else {
+			System.out.println("username is null or context is not null");
+		}
+		filterChain.doFilter(request, response);
 	}
 
 }
