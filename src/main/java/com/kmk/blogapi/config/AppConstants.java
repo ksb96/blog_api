e8986908c39223634858bdf6df36aa932a5f20c4
ksb96 <52975077+ksb96@users.noreply.github.com>
2024-03-07 14:39:06 +0530

    comments -updated

--- src/main/java/com/kmk/blogapi/config/AppConstants.java
+++ src/main/java/com/kmk/blogapi/config/AppConstants.java
@@ -2,7 +2,7 @@ package com.kmk.blogapi.config;
 
 public class AppConstants {
 	
-	//constants
+	//constants- pagination
 	public static final String PAGE_NUMBER = "0";
 	public static final String PAGE_SIZE = "5";
 	public static final String SORT_BY = "postId";
